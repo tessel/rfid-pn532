516599d111af6697131eb4788d29373be5a74ac2
Frijol <ifoundthemeaningoflife@gmail.com>
2014-05-23 12:51:40 -0700

    re-standardized

--- examples/rfid.js
+++ examples/rfid.js
@@ -1,18 +1,15 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
 /*********************************************
 This basic RFID example listens for an RFID
 device to come within range of the module,
 then logs its UID to the console.
 *********************************************/
 
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/publicdomain/zero/1.0/
-
 var tessel = require('tessel');
 
-var portname = process.argv[2] || 'A';
-var rfid = require('../').use(tessel.port[portname]); // Replace '../' with 'rfid-pn532' in your own code
-
-console.log('Using port', portname)
+var rfid = require('../').use(tessel.port['A']); // Replace '../' with 'rfid-pn532' in your own code
 
 rfid.on('ready', function (version) {
   console.log('Ready to read RFID card');
@@ -21,7 +18,3 @@ rfid.on('ready', function (version) {
     console.log('UID:', uid);
   });
 });
-
-rfid.on('error', function (err) {
-  console.log(err)
-})
