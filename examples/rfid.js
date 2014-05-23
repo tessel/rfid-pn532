d6fdc8692e5140e220cdc3e536c66af2d0f9632e
Frijol <ifoundthemeaningoflife@gmail.com>
2014-05-23 12:57:07 -0700

    added back error handling code

--- examples/rfid.js
+++ examples/rfid.js
@@ -1,11 +1,11 @@
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/publicdomain/zero/1.0/
-
 /*********************************************
 This basic RFID example listens for an RFID
 device to come within range of the module,
 then logs its UID to the console.
 *********************************************/
+
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
 
 var tessel = require('tessel');
 
@@ -17,4 +17,8 @@ rfid.on('ready', function (version) {
   rfid.on('data', function(uid) {
     console.log('UID:', uid);
   });
+});
+
+rfid.on('error', function (err) {
+  console.error(err);
 });
