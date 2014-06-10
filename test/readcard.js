77ef11d0b0db4080088676cd46187be42e250870
EvanSimpson <esimpson.rescue@gmail.com>
2014-06-09 17:19:46 -0700

    Fixing test file

--- test/readcard.js
+++ test/readcard.js
@@ -15,8 +15,8 @@ rfid.on('ready', function (version) {
 
   rfid.on('read', function(data) {
     console.log('# uid:', data.uidStr);
-    console.log(data.uid.length == 7 ? 'ok' : 'not ok', '- length of returned data');
-    // rfid.disable();
+    console.log(data.uid.length == 7 ? 'ok' : data.uid.length == 4 ? 'ok' : 'not ok', '- length of returned data');
+    rfid.disable();
   });
 });
 
